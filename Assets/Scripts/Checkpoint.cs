824c3c9a9f530eb7a631f697bba1b97c2af8f9f1
R3messa <r3messa@gmail.com>
2020-07-06 14:31:28 +0300

    checkpoint

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -24,6 +24,13 @@ public class Checkpoint : MonoBehaviour
         {
             GameManager.instance.SetSpawnPoint(transform.position);
 
+            Checkpoint[] allCP = FindObjectsOfType<Checkpoint>();
+            for(int i = 0; i < allCP.Length; i++)
+            {
+                allCP[i].cpOff.SetActive(true);
+                allCP[i].cpOn.SetActive(false);
+            }
+
             cpOff.SetActive(false);
             cpOn.SetActive(true);
         }
